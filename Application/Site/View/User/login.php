7650a384c31daa3ebf550281274d9dce5555efee
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-06-24 14:47:29 +0200

    Add login view

---
+++ Application/Site/View/User/login.php
@@ -0,0 +1,19 @@
+<div class="login">
+
+	<form action="login" method="post" style="text-align: center">
+		<label>
+			<h4>Pseudo</h4>
+			<input type="text" name="pseudo" class="btn btn-default" placeholder="Pseudo">
+		</label>
+		<br/>
+		<label>
+			<h4>Mot de passe</h4>
+			<input type="password" name="password" class="btn btn-default" placeholder="Mot de passe">
+		</label>
+		<br/>
+		<label>
+			<input type="submit" value="Se connecter" class="btn btn-success">
+		</label>
+	</form>
+
+</div>

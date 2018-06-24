874be89e11ed4580fc1425dfb89ea063b6cfb805
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-06-24 14:38:30 +0200

    Add router path for register

--- index.php
+++ index.php
@@ -8,5 +8,7 @@ $router->get('/theme/listing', function () { $themeListing = new \Application\Si
 $router->get('/theme/view/:name', function ($name) { $name = str_replace('-', ' ', $name); $themeListing = new \Application\Site\Controller\ThemeGallery();$themeListing->invokeThemePage($name); });
 $router->get('/user/listing', function () {});
 $router->get('/user/view/:name', function ($name) { $name = str_replace('-', ' ', $name); $userAccount = new \Application\Site\Controller\User();$userAccount->invokeViewAccountPage($name); });
+$router->get('/user/register', function () { $userAccount = new \Application\Site\Controller\User();$userAccount->invokeLoginPage(); });
+$router->post('user/register', function() { print_r($_POST); });
 
 $router->run();

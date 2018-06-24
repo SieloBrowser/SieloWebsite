e9c58774a70e9982795ec57eb6333d6413669683
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-06-24 19:29:10 +0200

    Minor changes

--- Application/Site/View/Default/default.php
+++ Application/Site/View/Default/default.php
@@ -1,32 +1,32 @@
 <html lang="en">
-<head>
-    <?php
-        $this->htmlDocument->header->addMetaTag(['name' => 'test']);
-        $this->htmlDocument->header->parseHeaderDatas();
-    ?>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-</head>
+    <head>
+        <?php
+            $this->htmlDocument->header->addMetaTag(['name' => 'test']);
+            $this->htmlDocument->header->parseHeaderDatas();
+        ?>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </head>
 
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header" style="width: 100%;">
-            <a class="navbar-brand" href="#"><?php echo $this->lang->getKey('TITLE'); ?></a>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header" style="width: 100%;">
+                    <a class="navbar-brand" href="#"><?php echo $this->lang->getKey('TITLE'); ?></a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+
+            <div class="starter-template" style="padding-top: 100px;">
+                <?php echo $includedContent; ?>
+            </div>
+
         </div>
-    </div>
-</nav>
-
-<div class="container">
-
-    <div class="starter-template" style="padding-top: 100px;">
-		<?php echo $content; ?>
-    </div>
-
-</div>
-</body>
+    </body>
 </html>

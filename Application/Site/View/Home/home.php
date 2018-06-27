5e176dbeb948725dbe07461f3ebe59e0a65b43d7
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-06-27 18:36:25 +0200

    [Add] => home view

---
+++ Application/Site/View/Home/home.php
@@ -0,0 +1,189 @@
+<div id="page-wrapper">
+
+	<!-- Header -->
+	<header id="header">
+		<h1 id="logo"><a href="index.html">Sielo</a></h1>
+		<nav id="nav">
+			<ul>
+				<li><i><a href="../fr/" class="scrolly">Français</a></i></li>
+				<li><a href="#tabspace" class="scrolly">Tabs spaces</a></li>
+				<li><a href="#floatingbutton" class="scrolly">Floating Button</a></li>
+				<li><a href="#download" class="button special scrolly">Download</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<!-- Banner -->
+	<section id="banner">
+		<div class="content">
+			<header>
+				<h2>Sielo</h2>
+				<p>See the web differently with Sielo's tabs spaces.</p>
+			</header>
+			<span class="image"><img src="../images/icon.png" alt="" /></span>
+		</div>
+		<a href="#one" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- One -->
+	<section id="one" class="spotlight style1 bottom">
+		<span class="image fit main"><img src="../images/official-sielo.png" alt="" /></span>
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<div class="4u 12u$(medium)">
+						<header>
+							<h2>Sielo, not just a web browser</h2>
+							<p>Sielo has all the functionnalities of a traditionnal web browser, but that's not the end! No more tabs everywhere, browse differently by using tabs spaces!</p>
+						</header>
+						<ul class="actions">
+							<li><a href="#download" class="button scrolly">Download</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a href="#tabspace" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Tabs spaces -->
+	<section id="tabspace" class="spotlight style2 left">
+		<span class="image fit main"><img src="../images/tabsspaces.png" alt="" /></span>
+		<div class="content">
+			<header>
+				<h2>Do more with tabs spaces</h2>
+				<p>"No more tabs everywhere"</p>
+			</header>
+			<p>This is the feature that makes Sielo so special. With tabs spaces you can divide your Sielo window into several spaces, as if you had several windows next to each other but still interacting with each others. You can resize your tabs spaces, hide them, mute them... This makes possible, among other things, to have several sessions, for example a session called "social networks", another called "music" and another called "work". Therefore you can hide the "music" tabs space and retrieve it at any time.</p>
+		</div>
+		<a href="#floatingbutton" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Floating button -->
+	<section id="floatingbutton" class="spotlight style3 right">
+		<span class="image fit main bottom"><img src="../images/fbutton.png" alt="" /></span>
+		<div class="content">
+			<header>
+				<h2>Floating buttons</h2>
+				<p>Be more efficient and gain place with floating button</p>
+			</header>
+			<p>This is another specific feature of Sielo, with it, you can gain space by dematerialising the browser's toolbar. Being a simple mobile button, it gives you access to all the needed controls during your browsing and can adapt itself to your workspace.</p>
+		</div>
+		<a href="#caracteristics" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- caracteristics -->
+	<section id="caracteristics" class="wrapper style1 special fade-up">
+		<div class="container">
+			<header class="major">
+				<h2>Start using Sielo now</h2>
+				<p>A project with the awesome help of Hotaru, LavaPower and Kaktus!</p>
+			</header>
+			<div class="box alt">
+				<div class="row uniform">
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-gift"></span>
+						<h3>Free</h3>
+						<p>Sielo is free, now and forever. It's a free open-source project started in 2016, with the purpose of improving your web experience</p>
+					</section>
+					<section class="4u 6u$(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-eye"></span>
+						<h3>Improved by Contributors</h3>
+						<p>Sielo is open-source. It means people like you can help us with development or design. That way we can all work to improve Sielo every days.</p>
+					</section>
+					<section class="4u$ 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-paint-brush"></span>
+						<h3>Customizable</h3>
+						<p>With its powerful theme API,  you can change Sielo's entire design with customizable themes.  You can also directly control theme's colors</p>
+					</section>
+					<section class="4u 6u$(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-rocket"></span>
+						<h3>Fast</h3>
+						<p>By using the latest version of the Blink engine, Sielo makes possible to browse quickly on the web.</p>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-heart"></span>
+						<h3>Help Us</h3>
+						<p>There are many ways to help us. You can <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MU7RV3SGKTFFS" target="_blank">make a donation</a>, help us improve the source code, or simply share and talk about Sielo to your family and friends.</p>
+					</section>
+					<section class="4u$ 6u$(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-umbrella"></span>
+						<h3>Update Regularly</h3>
+						<p>I am working night and day to improve Sielo and offer you the best updates as quick as possible.</p>
+					</section>
+				</div>
+			</div>
+			<footer class="major">
+			</footer>
+		</div>
+	</section>
+
+	<!-- Four -->
+	<section id="download" class="wrapper style1 special fade-up">
+		<div class="container">
+			<header class="major">
+				<h2>Download</h2>
+			</header>
+			<div class="box alt">
+				<div class="row uniform">
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-windows"></span>
+						<h3>Windows (64bit)</h3>
+						<ul class="actions">
+							<li><a href="../download.php?for=windows" class="button">Dowload Setup</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-windows"></span>
+						<h3>Windows (32bit)</h3>
+						<ul class="actions">
+							<li><a href="../download.php?for=windows_x86" class="button">Dowload Setup</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-windows"></span>
+						<h3>Windows</h3>
+						<ul class="actions">
+							<li><a href="../download.php?for=windows_portable" class="button">Dowload Portable Version</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-linux"></span>
+						<h3>Linux</h3>
+						<ul class="actions">
+							<li><a href="../download.php?for=linux" class="button">Download AppImage</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-linux"></span>
+						<h3>ArchLinux</h3>
+						<ul class="actions">
+							<li><a href="#" class="button">Available in the AUR</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u(medium) 12u$(xsmall)">
+						<span class="icon alt major fa-github"></span>
+						<h3>GitHub</h3>
+						<ul class="actions">
+							<li><a href="https://github.com/Feldrise/SieloNavigateur" class="button" target="_blank">Source code</a></li>
+						</ul>
+					</section>
+				</div>
+			</div>
+		</div>
+		<a href="#download" class="goto-next scrolly">Next</a>
+	</section>
+
+	<!-- Footer -->
+	<footer id="footer">
+		<ul class="icons">
+			<li><a href="https://twitter.com/SieloBrowser" class="icon alt fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+			<li><a href="https://instagram.com/sielo.browser" class="icon alt fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+			<li><a href="https://www.facebook.com/SieloBrowser/" class="icon alt fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+			<li><a href="https://github.com/SieloBrowser/SieloBrowser" class="icon alt fa-github" target="_blank"><span class="label">GitHub</span></a></li>
+			<li><a href="mailto:admin@feldrise.com" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+		</ul>
+		<ul class="copyright">
+			<li>&copy; Victor "Feldrise" DENIS. All rights reserved.</li><li>Design: <a href="http://twitter.com/ajlkn">AJ</a></li>
+		</ul>
+	</footer>

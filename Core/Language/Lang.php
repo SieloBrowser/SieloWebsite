1d8e109293f4dee055a8a9e4bfe3042abd95e44d
Kakktuss <benjaminmanderveldepro@gmail.com>
2018-07-11 16:47:19 +0200

    [Refactor] => Lang core class

--- Core/Language/Lang.php
+++ Core/Language/Lang.php
@@ -11,54 +11,84 @@ namespace Core\Language;
 class Lang
 {
 
-	private $lang;
-	private $sitePart;
-	private $fileName;
-	public $commonLangFile;
-	public $defaultLangFile;
+    private $lang;
+    private $basePart = null;
+    private $files = [];
 
-	public function __construct($lang, $fileName, $sitePart)
-	{
-		$this->lang = $lang;
-		$this->fileName = $fileName;
-		$this->sitePart = $sitePart;
-		$this->getLangFile();
-	}
+    public function __construct(string $lang, string $basePart = null)
+    {
+        $this->lang = $lang;
+        $this->basePart = $basePart;
+        if(!is_null($basePart))
+            $this->files['default'] = ["file" => fopen($_SERVER['DOCUMENT_ROOT'].'/Sielo/Application/'.$basePart.'/Langs/'.$lang.'/default.ini', 'r'), "use" => true];
+    }
 
-	public function getKey($keyName)
-	{
-		for($i = 1; $i <= 2; $i++)
-		{
-			if($i === 1)
-				$file = $this->defaultLangFile;
-			else
-				$file = $this->commonLangFile;
-			while (($buffer = fgets($file)) !== false)
-			{
-				$currentKey = substr($buffer, 0, strpos($buffer, '='));
-				if(strpos($buffer, $keyName) !== false && strlen($keyName) === strlen($currentKey))
-				{
-					$start = strpos($buffer, '=');
-					$start++;
-					$value = substr($buffer, $start, strlen($buffer));
+    public function addFile(string $fileName, string $part = null)
+    {
+        if(!is_null($this->basePart))
+        {
+            $filePath = $_SERVER['DOCUMENT_ROOT'].'/Sielo/Application/'.((isset($part)) ? $part : $this->basePart).'/Langs/'.$this->lang.'/'.$fileName.'.ini';
+            if(file_exists($filePath))
+            {
+                $this->files[$fileName] = ["file" => fopen($filePath, 'r'), "use" => true];
+            } else {
+                throw new LangException('[Lang] {addFile} => file given: '.$fileName.' doesn\'t exists');
+            }
+        } else {
+            throw new LangException('[Lang] {addFile} => Base site part is not set. Please give a part for search the file');
+        }
+    }
 
-					fseek($this->commonLangFile, 0);
-					fseek($this->defaultLangFile, 0);
-					return $value;
-				}
-			}
-		}
+    public function useFile(string $fileName, bool $use)
+    {
+        if($this->fileExists($fileName))
+        {
+            $this->files[$fileName]['use'] = $use;
+        } else {
+            throw new LangException('[Lang] {useFile} => File given doesn\'t exists');
+        }
+    }
 
-		fseek($this->commonLangFile, 0);
-		fseek($this->defaultLangFile, 0);
-		return false;
-	}
+    public function deleteFile($fileName)
+    {
+        if($this->fileExists($fileName))
+        {
+            unset($this->files[$fileName]);
+        } else {
+            throw new LangException('[Lang] {deleteFile} => File given doesn\'t exists');
+        }
+    }
 
-	private function getLangFile()
-	{
-		$commonLangFile = fopen($_SERVER['DOCUMENT_ROOT'].'/Sielo/Application/'.$this->sitePart.'/Langs/'.$this->lang.'/'.$this->fileName.'.ini', 'r');
-		$defaultLangFile = fopen($_SERVER['DOCUMENT_ROOT'].'/Sielo/Application/'.$this->sitePart.'/Langs/'.$this->lang.'/default.ini', 'r');
-		$this->commonLangFile = $commonLangFile;
-		$this->defaultLangFile = $defaultLangFile;
-	}
+    public function getKey($keyName)
+    {
+        foreach ($this->files as $file)
+        {
+            if($file['use'] === true)
+            {
+                $file = $file['file'];
+                while(($buffer = fgets($file)) !== false)
+                {
+                    $currentKey = substr($buffer, 0, strpos($buffer, '='));
+                    if(strpos($buffer, $keyName) !== false && $keyName === $currentKey)
+                    {
+                        $start = strpos($buffer, '=');
+                        $start++;
+                        $value = substr($buffer, $start, strlen($buffer));
+
+                        return $value;
+                    }
+                }
+            }
+
+            fseek($file, 0);
+        }
+        return false;
+    }
+
+    private function fileExists(string $fileName)
+    {
+        if(isset($this->files[$fileName]))
+            return true;
+        return false;
+    }
 }
